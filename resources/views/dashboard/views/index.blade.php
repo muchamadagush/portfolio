ac3ef2effcd7452300357a23b5233175ebbfc9c6
Muchamad Agus Hermawan <muchamadagushermawan98@gmail.com>
2020-12-15 09:23:38 +0700

    membuat halaman home

---
+++ resources/views/dashboard/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.myLayouts', ['title' => 'Muchamad Agus Hermawan'])
+
+@section('content')
+  <div class="jumbotron">
+    <div class="container">
+      <h1 class="display-4">Hello, I'm<br><span>Muchamad<br>Agus Hermawan</span></h1>
+    </div>
+  </div>   
+@endsection
