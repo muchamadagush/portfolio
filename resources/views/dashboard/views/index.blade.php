b1e2777ead115f092850158a48572639fe396ca7
Muchamad Agus Hermawan <muchamadagushermawan98@gmail.com>
2021-01-01 00:38:37 +0700

    re-design page home dan perbaikan media query

--- resources/views/dashboard/views/index.blade.php
+++ resources/views/dashboard/views/index.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.myLayouts', ['title' => 'Muchamad Agus Hermawan'])
 
 @section('content')
-  <div class="jumbotron">
-    <div class="container">
-      <h1 class="display-4">Hello, I'm<br><span>Muchamad<br>Agus Hermawan</span></h1>
-    </div>
-  </div>   
+<div class="container">
+  <div id="index">
+    <h1 class="text-center align-middle">MUCHAMAD AGUS HERMAWAN</h1>
+  </div>
+</div>
 @endsection
